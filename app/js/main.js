825fac4010002aa21d68385ce5bf928dd59a9ab3
Kermishkov1996 <gtytr.n@yandex.ru>
2025-02-16 20:40:59 +0400

    advantages open content js

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,34 @@
-$(function () {
+/*$('.item__button').on('click', function(){
+	$(this).toggleClass('is-active');
+});*/
 
-})
+const itemBtn = document.querySelector('.item__button');
+const body = document.querySelector('body');
+
+//открывает меню при нажатии на бургер
+itemBtn.addEventListener('click', ()=>{
+	itemBtn.classList.toggle('is-active');
+	body.classList.remove('is-active');
+});
+/*
+
+//функция для закрытия меню
+function close() {
+	menuList.classList.remove('menu__list--open');
+	menuShadow.classList.remove('menu--open');
+	body.classList.remove('menu--scroll'); //блокирует скролл в открытом меню
+}
+
+//закрывает меню при нажатии на крестик в меню
+menuClose.addEventListener('click', ()=>{
+	close();
+});
+
+//закрывает меню при нажатии на ссылку в меню
+menuList.addEventListener('click', (event)=>{
+	if(event.target.tagName === "A")
+		close();
+});
+
+//закрывает меню при нажатии на поле за пределами меню
+menuShadow.addEventListener('click', close);*/
